49f8b6a9f7ef8e72df89af751dc5b0f8cd01d76c
Sami Fink <fink.s.m.8651@gmail.com>
2024-05-20 23:59:41 +0000

    feat: added error conditions

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,23 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      const {text, locale} = req.body
+      const {text, locale} = req.body;
+      
+      if (text === "" | !locale) {
+        res.json({error: "Required field(s) missing"});
+        return;
+      }
+
+      if (!text.trim()) {
+        res.json({error: "No text to translate"});
+        return;
+      }
+
+      if (locale !== "american-to-british" & locale !== "british-to-american") {
+        res.json({error: "Invalid value for locale field"});
+        return;
+      }
+
       const translation = translator.translate(text, locale);
       if (text === translation) {
         res.json({text, translation: "Everything looks good to me!"});
