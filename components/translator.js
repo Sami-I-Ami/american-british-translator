54beb230d9de36bf60c6df00293a2a803ce61326
Sami Fink <fink.s.m.8651@gmail.com>
2024-05-20 09:45:18 +0000

    feat: added highlight method

--- components/translator.js
+++ components/translator.js
@@ -4,7 +4,18 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
+    constructor(text, locale) {
+        this.text = text;
+        this.locale = locale;
+    }
 
+    highlight(words) {
+        return `<span class="highlight">${words}</span>`
+    }
+
+    translate() {
+
+    }
 }
 
 module.exports = Translator;
